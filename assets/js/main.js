c6a99380c1b3a69a5f53d2b29b0e4b0f80892944
alemtt <ale@mattera.eu>
2024-02-16 15:38:03 +0100

    final commit esercizio 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,25 @@ console.log(PI)
 
 // Esercizio 2/B
 
-// const PI = 3.1416 da errore di sintassi perchè 'const', viene utilizzato per assegnare una constante e non una variabile che puo modificarsi
+/*
+const PI = 3.1416 da errore di sintassi perchè 'const'
+viene utilizzato per assegnare una constante e 
+non una variabile che puo modificarsi
 
+*/
 // Esercizio 2/C
 
 let radius = 8 //number
 
-let circle = (radius*2)*PI //string
+let circle = (radius * 2) * PI //string
 console.log(circle)
+
+// Esercizio 3
+
+let name = 'Carlo'; //string
+
+console.log(name)
+
+name = "Marco" //string
+
+console.log(name)
