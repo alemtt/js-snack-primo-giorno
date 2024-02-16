1720b7d2366a7300a5884ce175b46397e2fc163e
alemtt <ale@mattera.eu>
2024-02-16 15:20:37 +0100

    add esercizio 2/b

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 // Esercizio 1
 
-let number = 100 ; //number
+let number = 100; //number
 console.log(number)
 
 // Esercizio 2
 
-const PI = 3.14 ; //number
+const PI = 3.14; //number
 console.log(PI)
+
+// Esercizio 2/B
+
+// const PI = 3.1416 da errore di sintassi perchè 'const', viene utilizzato per assegnare una constante e non una variabile che puo modificarsi
+
+// Esercio
